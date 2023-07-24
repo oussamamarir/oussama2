1cbd0141af4a950029716ddd6227dc1dd272296e
marir oussama <121771050+oussamamarir@users.noreply.github.com>
2023-07-24 11:08:29 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'black-t-50':'rgba(0,0,0,0.5'
+      }
+    },
+  },
+  plugins: [],
+}
+
